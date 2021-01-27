a3b773e6b321c671630a39a95fd8832710090ac5
emansawalha <emansawalha10@gmail.com>
2021-01-27 16:19:29 +0200

    js prompt

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var user_level = prompt ('Are You biginner in HTML')
-if(user_level=== true){
-    document.write('<h3>' + 'Please Read This page carefully' +</h3>)
+var user_level = prompt ('Are You biginner in HTML');
+alert("Welcome to HTML tutorial")
+
+
+if(user_level === 'yes'){
+
+    document.write ('<h2>' + 'Please Read This page carefully' +'</h2>')
+    alert(" please Read This page carefully ")
 
 }
-else if (user_level === false){
-    document.write('Move to Live tutorial ')
+
+else if(user_level != 'yes' || "") {
+    document.write('<h2>' + "you can find useful tutorial links in this webpage "+'</h3>')
+
+    alert("you can find useful tutorials in this webpage");
 }
-else {
-    alert("Yoy can find samples of wesites")
-}
+   
+   
+   
+
