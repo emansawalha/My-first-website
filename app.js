78304466427361ecc2fdaea9d372a21a36962218
emansawalha <emansawalha10@gmail.com>
2021-01-27 15:33:26 +0200

    js app

---
+++ app.js
@@ -0,0 +1,11 @@
+var user_level = prompt ('Are You biginner in HTML')
+if(user_level=== true){
+    document.write('<h3>' + 'Please Read This page carefully' +</h3>)
+
+}
+else if (user_level === false){
+    document.write('Move to Live tutorial ')
+}
+else {
+    alert("Yoy can find samples of wesites")
+}
